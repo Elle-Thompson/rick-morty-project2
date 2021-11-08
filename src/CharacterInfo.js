6acbb7bde8c88b060b201ad7aa3e32e9ca979ae7
Elle Thompson <elletee@Elles-iMac.attlocal.net>
2021-11-08 09:24:13 -0800

    fixed bugs in charcacterInfo component

--- src/CharacterInfo.js
+++ src/CharacterInfo.js
@@ -4,54 +4,34 @@ import { useEffect, useState } from "react";
 import Card from "react-bootstrap/Card";
 
 function CharacterInfo(props) {
-
-
-
   // let {info} = useParams ();
- 
+
   const [characterInfoBox, setCharacterInfoBox] = useState({});
 
+  const character = props.match.params.info;
 
-
-
-
-const character  = props.match.params.info;
-
-  const apiInfo =  () => {
-      fetch(`https://rickandmortyapi.com/api/character/${character}`)
-      .then((res) => res.json())
-      .then((data) => setCharacterInfoBox(data));
- 
+  const apiInfo = async () => {
+    const apiResult = await fetch(`https://rickandmortyapi.com/api/character/${character}`)
+    const json = await apiResult.json ();
+    setCharacterInfoBox(json)
+      // .then((res) => res.json())
+      // .then((data) => setCharacterInfoBox(data));
   };
 
- useEffect(() => {
+  useEffect(() => {
     apiInfo();
-  }, {} );
-
+  }, []);
 
   console.log(characterInfoBox);
 
-   
-
-
- 
-
- 
-
   return (
-    
-      <div className="charInfo-wrapper">
+    <div className="charInfo-wrapper">
       <Link className="portal-gun" exact to="/CharacterSearch">
         <img
           className="pg1"
           src="https://i.ibb.co/tXtBq44/rick-and-morty-rick-sanchez-portal-gun-custom-cursor.png"
         />
       </Link>
-
-
-     
-     
-
 
       <Card className="info-cards" style={{ width: "30rem" }}>
         <Card.Img
@@ -61,25 +41,38 @@ const character  = props.match.params.info;
           alt=""
         />
         <Card.Body>
-          <Card.Title className="card-title" > <span>Name:</span> {characterInfoBox.name}</Card.Title>
+          <Card.Title className="card-title">
+            {" "}
+            <span>Name:</span> {characterInfoBox.name}
+          </Card.Title>
           <Card.Text>
-            <p><span> Species:</span> {characterInfoBox.species} </p>
-            <p> <span>Gender:</span> {characterInfoBox.gender} </p>
-            <p> <span>Type:</span> {characterInfoBox.type} </p>
-            <p> <span>Current Status:</span> {characterInfoBox.status} </p>
-            {/* <p> Episode: {characterInfoBox.episode} </p> */}
-              {/* <p> Origin Location: {characterInfoBox.origin[0]} </p> */}
-            {/* <p> Current Location:  {characterInfoBox.location.name}    </p> */}
+            <p>
+              <span> Species:</span> {characterInfoBox.species}{" "}
+            </p>
+            <p>
+              {" "}
+              <span>Gender:</span> {characterInfoBox.gender}{" "}
+            </p>
+            <p>
+              {" "}
+              <span>Type:</span> {characterInfoBox.type}{" "}
+            </p>
+            <p>
+              {" "}
+              <span>Current Status:</span> {characterInfoBox.status}{" "}
+            </p>
+            <p> <span>Origin Location: </span>{characterInfoBox.origin ? characterInfoBox.origin.name : ""} </p>
+            <p> <span>Current Location: </span>  {characterInfoBox.location ? characterInfoBox.location.name : ""}    </p>
+            <p> <span>First Episode: </span>{characterInfoBox.episode ? characterInfoBox.episode[0] : ""} </p>
+            <p> <span>Last Episode: </span>{characterInfoBox.episode ? characterInfoBox.episode[characterInfoBox.episode.length -1] : ""} </p>
+            
             <p> </p>
           </Card.Text>
         </Card.Body>
       </Card>
-
-      </div>
-
-      
-    
+    </div>
   );
 }
 
 export default CharacterInfo;
+
